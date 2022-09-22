48758f012b4fccb2f9ff20334ce5f387909b76b0
FranciscoDiego64 <112688191+FranciscoDiego64@users.noreply.github.com>
2022-09-22 14:05:07 +0200

    Added Pokemon objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
-/* math example
-let size = 100;
-let doubleSize = size * 2;
-let minSize = (doubleSize * 2) - (size / 2);
-document.write(minSize);
-*/
-let favoriteFood = 'enchiladas de mole';
-document.write(favoriteFood);
-alert('Hello world');
+let pokemonList = [
+    {name: 'Bulbasaur', height: 0.7, type: ['grass', 'poison'], pokedexNumber: 001},
+    {name: 'Sandshrew', height: 0.6, type: 'ground', pokedexNumber: 027},
+    {name: 'Nidoqueen', height: 1.3, type: ['poison', 'ground'], pokedexNumber: 031},
+    {name: 'Abra', height: 0.9 , type: 'psychic', pokedexNumber: 063},
+    {name: 'Exeggutor', height: 2, type: ['grass', 'psychic'], pokemonNumber: 103},
+    {name: 'Ditto', height: 0.3, type: 'normal', pokemonNumer: 132},
+    {name: 'Dragonite', height: 2.2, type: ['dragon', 'flying'], pokedexNumber: 149},
+]
