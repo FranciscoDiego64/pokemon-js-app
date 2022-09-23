14d846b3543d5f1b72cc55a7b3632718e1f9a294
FranciscoDiego64 <112688191+FranciscoDiego64@users.noreply.github.com>
2022-09-23 15:19:28 +0200

    Added for loop in pokemon array

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,16 @@ let pokemonList = [
     {name: 'Ditto', height: 0.3, type: 'normal', pokemonNumer: 132},
     {name: 'Dragonite', height: 2.2, type: ['dragon', 'flying'], pokedexNumber: 149},
 ]
+
+for (let i=0; i<=pokemonList.length; i++) {
+    if (pokemonList[i].height >1.0 && pokemonList[i].height <2.0) {
+     document.write(pokemonList[i].name + " " + "(" + "height:"+ " " + pokemonList[i].height + "m" + ")" + " - wow that's big!");
+      document.write("<br>");
+   } else if (pokemonList[i].height >=2.0) {
+      document.write(pokemonList[i].name + " " + "(" + "height:"+ " " + pokemonList[i].height + "m" + ")" + " - omg that's REALLY big!")
+      document.write("<br>");
+   } else {
+      document.write(pokemonList[i].name + " " + "(" + "height:"+ " " + pokemonList[i].height + "m" + ")");
+      document.write("<br>");
+   }
+  }
