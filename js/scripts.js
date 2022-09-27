07f77c0d7a2eb36af78cf3813bd099ada8909391
FranciscoDiego64 <112688191+FranciscoDiego64@users.noreply.github.com>
2022-09-27 16:38:46 +0200

    fixed typing errors in array

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,10 @@
 let pokemonList = [
-    {name: 'Bulbasaur', height: 0.7, type: ['grass', 'poison'], pokedexNumber: 001},
-    {name: 'Sandshrew', height: 0.6, type: 'ground', pokedexNumber: 027},
-    {name: 'Nidoqueen', height: 1.3, type: ['poison', 'ground'], pokedexNumber: 031},
-    {name: 'Abra', height: 0.9 , type: 'psychic', pokedexNumber: 063},
-    {name: 'Exeggutor', height: 2, type: ['grass', 'psychic'], pokemonNumber: 103},
-    {name: 'Ditto', height: 0.3, type: 'normal', pokemonNumer: 132},
+    {name: 'Bulbasaur', height: 0.7, type: ['grass', 'poison'], pokedexNumber: 1},
+    {name: 'Sandshrew', height: 0.6, type: 'ground', pokedexNumber: 27},
+    {name: 'Nidoqueen', height: 1.3, type: ['poison', 'ground'], pokedexNumber: 31},
+    {name: 'Abra', height: 0.9 , type: 'psychic', pokedexNumber: 63},
+    {name: 'Exeggutor', height: 2, type: ['grass', 'psychic'], pokedexNumber: 103},
+    {name: 'Ditto', height: 0.3, type: 'normal', pokedexNumber: 132},
     {name: 'Dragonite', height: 2.2, type: ['dragon', 'flying'], pokedexNumber: 149},
 ]
 
