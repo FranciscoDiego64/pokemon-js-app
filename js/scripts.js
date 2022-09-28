874dbc5c6d3b32b9f14b9e009c2577c2c75f192d
FranciscoDiego64 <112688191+FranciscoDiego64@users.noreply.github.com>
2022-09-28 13:09:40 +0200

    Wrapped pokemonList array in IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -22,6 +22,38 @@ for (let i=0; i<=pokemonList.length; i++) {
   }
   */
 
+  //IIFE
+let pokemonRepository = (function (){
+
+    let pokemonList = [
+      {name: 'Bulbasaur', height: 0.7, type: ['grass', 'poison'], pokedexNumber: 1},
+      {name: 'Sandshrew', height: 0.6, type: 'ground', pokedexNumber: 27},
+      {name: 'Nidoqueen', height: 1.3, type: ['poison', 'ground'], pokedexNumber: 31},
+      {name: 'Abra', height: 0.9 , type: 'psychic', pokedexNumber: 63},
+      {name: 'Exeggutor', height: 2, type: ['grass', 'psychic'], pokedexNumber: 103},
+      {name: 'Ditto', height: 0.3, type: 'normal', pokedexNumber: 132},
+      {name: 'Dragonite', height: 2.2, type: ['dragon', 'flying'], pokedexNumber: 149},
+  ]
+  
+    function getAll () {
+      return pokemonList;
+    }
+    function add (pokemon) {
+      pokemonList.push(pokemon);
+    }
+  
+    return {
+      getAll: getAll,
+      add: add
+    }
+    
+  })()
+  
+  console.log(pokemonRepository.getAll());
+  pokemonRepository.add({name: "Vulpix", height: 0.6, type: 'fire', pokedexNumber: 37});
+  pokemonRepository.add({name: "Jigglypuff", height: 0.5, type: ['normal', 'fairy'], pokedexNumber: 39});
+
+
 // forEach refactoring
   pokemonList.forEach(function(pokemon){
     if (pokemon.height >1.0 && pokemon.height <2.0) {
