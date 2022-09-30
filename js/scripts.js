15a34460f4ae16e64fb59a6b9b078c78c127ebc2
FranciscoDiego64 <112688191+FranciscoDiego64@users.noreply.github.com>
2022-09-30 16:48:01 +0200

    Added new functions, see description for details
    Added elements to DOM.
    Added event listener for buttons.
    Added new functions.
    Deleted unused code.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,7 @@
-let pokemonList = [
+//IIFE
+let pokemonRepository = (function (){
+
+  let pokemonList = [
     {name: 'Bulbasaur', height: 0.7, type: ['grass', 'poison'], pokedexNumber: 1},
     {name: 'Sandshrew', height: 0.6, type: 'ground', pokedexNumber: 27},
     {name: 'Nidoqueen', height: 1.3, type: ['poison', 'ground'], pokedexNumber: 31},
@@ -7,63 +10,52 @@ let pokemonList = [
     {name: 'Ditto', height: 0.3, type: 'normal', pokedexNumber: 132},
     {name: 'Dragonite', height: 2.2, type: ['dragon', 'flying'], pokedexNumber: 149},
 ]
-/*
-for (let i=0; i<=pokemonList.length; i++) {
-    if (pokemonList[i].height >1.0 && pokemonList[i].height <2.0) {
-     document.write(pokemonList[i].name + " " + "(" + "height:"+ " " + pokemonList[i].height + "m" + ")" + " - wow that's big!");
-      document.write("<br>");
-   } else if (pokemonList[i].height >=2.0) {
-      document.write(pokemonList[i].name + " " + "(" + "height:"+ " " + pokemonList[i].height + "m" + ")" + " - omg that's REALLY big!")
-      document.write("<br>");
-   } else {
-      document.write(pokemonList[i].name + " " + "(" + "height:"+ " " + pokemonList[i].height + "m" + ")");
-      document.write("<br>");
+
+  function getAll () {
+    return pokemonList;
+  }
+  
+  function add (pokemon) { //(pokemon) = entry, item, pokemonEntry, etc.
+    if (typeof pokemon === 'object' && 'name', 'height' in pokemon) {
+    pokemonList.push(pokemon);
+    }else{
+    alert("this is not an object or info is missing")
    }
   }
-  */
 
-  //IIFE
-let pokemonRepository = (function (){
+  function showDetails(pokemon) {
+    console.log(pokemon);
+  }
 
-    let pokemonList = [
-      {name: 'Bulbasaur', height: 0.7, type: ['grass', 'poison'], pokedexNumber: 1},
-      {name: 'Sandshrew', height: 0.6, type: 'ground', pokedexNumber: 27},
-      {name: 'Nidoqueen', height: 1.3, type: ['poison', 'ground'], pokedexNumber: 31},
-      {name: 'Abra', height: 0.9 , type: 'psychic', pokedexNumber: 63},
-      {name: 'Exeggutor', height: 2, type: ['grass', 'psychic'], pokedexNumber: 103},
-      {name: 'Ditto', height: 0.3, type: 'normal', pokedexNumber: 132},
-      {name: 'Dragonite', height: 2.2, type: ['dragon', 'flying'], pokedexNumber: 149},
-  ]
+  function addListItem(pokemon) {
+    let pokemonListContainer = document.querySelector('.pokemon-list');
+    let listItem = document.createElement('li');
+    let pokeButton = document.createElement('button');
+    pokeButton.innerText = pokemon.name;
+    pokeButton.classList.add('pokebutton-class');
   
-    function getAll () {
-      return pokemonList;
-    }
-    function add (pokemon) {
-      pokemonList.push(pokemon);
-    }
-  
-    return {
-      getAll: getAll,
-      add: add
-    }
-    
-  })()
-  
-  console.log(pokemonRepository.getAll());
-  pokemonRepository.add({name: "Vulpix", height: 0.6, type: 'fire', pokedexNumber: 37});
-  pokemonRepository.add({name: "Jigglypuff", height: 0.5, type: ['normal', 'fairy'], pokedexNumber: 39});
+    listItem.appendChild(pokeButton);  
+    pokemonListContainer.appendChild(listItem); 
 
-
-// forEach refactoring
-  pokemonList.forEach(function(pokemon){
-    if (pokemon.height >1.0 && pokemon.height <2.0) {
-       document.write(pokemon.name + " " + "(" + "height:"+ " " + pokemon.height + "m" + ")" + " - wow that's big!");
-        document.write("<br>");
-     } else if (pokemon.height >=2.0) {
-        document.write(pokemon.name + " " + "(" + "height:"+ " " + pokemon.height + "m" + ")" + " - omg that's REALLY big!")
-        document.write("<br>");
-     } else {
-        document.write(pokemon.name + " " + "(" + "height:"+ " " + pokemon.height + "m" + ")");
-        document.write("<br>");
-     }
+    pokeButton.addEventListener('click', function (event) { //any name works
+    showDetails(pokemon);
     });
+  }
+  
+  return {
+    getAll: getAll,
+    add: add,
+    addListItem: addListItem,
+    showDetails: showDetails
+  }
+  
+})()
+
+//console.log(pokemonRepository.getAll());
+pokemonRepository.add({name: "Vulpix", height: 0.6, type: 'fire', pokedexNumber: 37});
+pokemonRepository.add({name: "Jigglypuff", height: 0.5, type: ['normal', 'fairy'], pokedexNumber: 39});
+// console.log(pokemonList[0].name); will be error. Cannot access "pokemonList" because it's inside the IIFE
+
+pokemonRepository.getAll().forEach(function(pokemon){
+  pokemonRepository.addListItem(pokemon);
+});
